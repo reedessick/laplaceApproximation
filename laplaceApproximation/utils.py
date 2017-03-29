458b3c861da3d04328adfd777a3f50f51e9b7783
Reed Essick <reed.essick@ligo.org>
2017-03-29 08:00:46 -0400

    added in some basic scripts as a skeleton. No real implementation yet

---
+++ laplaceApproximation/utils.py
@@ -0,0 +1,8 @@
+__description__ = "a module that houses convenient functions for the Laplace approximation of distributions of BayesFactors along with marginalization"
+__author__ = "Reed Essick"
+
+#-------------------------------------------------
+
+import numpy as np
+
+#-------------------------------------------------
