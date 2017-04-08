9c882084e988833861c197c59d2c7cafbd1dcc78
Reed Essick <reed.essick@ligo.org>
2017-04-07 20:34:00 -0400

    continued work on marginalization over sampling errors

--- README.md
+++ README.md
@@ -17,15 +17,21 @@ This formalism should allow us to determine the expected distributions for any "
     - malmquist
     - background
   - write marginalization/convolution over sampling errors
+    - these are somewhat implemented, but either buggy or subject to numerical errors or very expensive
+  - write MCMC to sample distribution
+    - could be faster to compute than a grid-based solution (which I'm currently doing)
   - fitting routine
     - take in a list of samples and EM algorithm our way to a best fit distribution?
     - will need to profile this, possibly optimize. Could easily get very expensive.
+  - regression of the distributions of rhoA2o, rhoB2o given lnBSN, lnBCI
+    - not sure this is actually interesting...
 
 ### distrib_sanityCheck
 
   - write posteriors to disk in a recoverable way (pkl? npy?)
   - overlay expected scaling (parameterized by rho2) at various values of eta2/rho2
   - include effects of sampling errors
+    - some of this is in place, but it should be extended as I debug/write more ways to marginalize
 
 --------------------------------------------------
 
--- laplaceApprox/utils.py
+++ laplaceApprox/utils.py
@@ -15,10 +15,19 @@ def sumLogs( arrayLike, axis=0 ):
     '''
     return ln(sum(np.exp(arrayLike))) to high accuracy
     '''
-    maxVal = np.max(arrayLike, axis=axis)
-    ans = maxVal + np.log(np.sum(np.exp(np.array(arrayLike)-maxVal), axis=axis))
+    ### transpose to ensure axis=0
+    ### this just makes all the broadcasting stuff work better below
+    if axis!=0:
+        axes = range(len(np.shape(arrayLike)))
+        axes.insert( 0, axes.pop(axis)) ### there might be a more graceful way to do this...
+        arrayLike = np.transpose(arrayLike, axes=axes)    
+
+    maxVal = np.max(arrayLike, axis=0)
+    ans = maxVal + np.log(np.sum(np.exp(np.array(arrayLike)-maxVal), axis=0))
+
     if len(np.shape(arrayLike)) > 1:
         ans[maxVal==-np.infty] = -np.infty ### do this to avoid nan's from "np.infty-np.infty"
+
     return ans 
 
 #-------------------------------------------------
@@ -113,7 +122,9 @@ def lnProb_rhoA2rhoB2_given_rhoA2orhoB2o( rhoA2, rhoB2, rhoA2o, rhoB2o ):
 
     NOTE: essentially 2 independent chi2 distributions
     '''
-    if isinstance(rhoA2, (int,float)) and isinstance(rhoB2, (int,float)) and isinstance(rhoA2o, (int,float)) and isinstance(rhoB2o, (int,float)):
+    types = (int,float)
+    if isinstance(rhoA2, types) and isinstance(rhoB2, types) \
+      and isinstance(rhoA2o, types) and isinstance(rhoB2o, types):
         return np.log(0.25) - 0.5*(rhoA2 + rhoB2 + rhoA2o + rhoB2o) \
             + float(mpmath.log(mpmath.besseli(0, (rhoA2*rhoA2o)**0.5))) \
             + float(mpmath.log(mpmath.besseli(0, (rhoB2*rhoB2o)**0.5)))
@@ -132,6 +143,9 @@ def lnProb_rhoA2rhoB2_given_rhoA2orhoB2o( rhoA2, rhoB2, rhoA2o, rhoB2o ):
         if isinstance(rhoA2, np.ndarray) or isinstance(rhoB2, np.ndarray) or isinstance(rhoA2o, np.ndarray) or isinstance(rhoB2o, np.ndarray):
             ans = np.array(ans)
 
+        ans[ans!=ans] = -np.infty ### get rid of nans
+                              ### FIXME we may want to be smarter about this and 
+                              ### prevent nans from showing up in the first place
         return ans
 
 def lnProb_rho2eta2_given_rhoA2orhoB2o( rho2, eta2, rhoA2o, rhoB2o ):
@@ -146,8 +160,13 @@ def lnProb_rho2eta2_given_rhoA2orhoB2o( rho2, eta2, rhoA2o, rhoB2o ):
     ab = lnProb_rhoA2rhoB2_given_rhoA2orhoB2o( rhoA2, rhoB2, rhoA2o, rhoB2o )
     ba = lnProb_rhoA2rhoB2_given_rhoA2orhoB2o( rhoB2, rhoA2, rhoA2o, rhoB2o )
 
-    return 0.5*(np.log(rho2) - np.log(rho2 - 4*eta2)) \
-        + sumLogs( [ab, ba] )    
+    ans = 0.5*(np.log(rho2) - np.log(rho2 - 4*eta2)) \
+        + sumLogs( [ab, ba] )
+
+    ans[ans!=ans] = -np.infty ### get rid of nans
+                              ### FIXME we may want to be smarter about this and 
+                              ### prevent nans from showing up in the first place
+    return ans
 
 def lnProb_lnBSNlnBCI_given_rhoA2orhoB2o( lnBSN, lnBCI, rhoA2o, rhoB2o, params, f_tol=1e-10 ):
     '''
@@ -158,9 +177,14 @@ def lnProb_lnBSNlnBCI_given_rhoA2orhoB2o( lnBSN, lnBCI, rhoA2o, rhoB2o, params, 
     rho2 = lnBSN_to_rho2( lnBSN, params, f_tol=f_tol )
     eta2 = lnBCI_to_eta2( lnBCI, params )
 
-    return np.log(rho2) - np.log(params.c_bsn + 0.5*rho2) \
+    ans = np.log(rho2) - np.log(params.c_bsn + 0.5*rho2) \
         + np.log(eta2) - np.log(params.c_bci) \
         + lnProb_rho2eta2_given_rhoA2orhoB2o( rho2, eta2, rhoA2o, rhoB2o )
+
+    ans[ans!=ans] = -np.infty ### get rid of nans
+                              ### FIXME we may want to be smarter about this and 
+                              ### prevent nans from showing up in the first place
+    return ans
 
 #--- marginal distributions
 
@@ -342,7 +366,71 @@ def lnFFTProb_samplingError( atled, sigma ):
     '''
     return -2*(np.pi*sigma*atled)**2
 
-def convolve_samplingErrors( lnBSN, lnBCI, lnProb, rhoA2o, rhoB2o, params, frac=0.01, tukey_alpha=0.1 ):
+def slow_convolve_samplingErrors( lnBSN, lnBCI, rhoA2o, rhoB2o, params, frac=0.01, tukey_alpha=0.1, f_tol=1e-10 ):
+    '''
+    NOT IMPLEMENTED
+
+    we should take the fft and ifft of only one dimension at a time and do this by hand.
+    this should allow us to determine whether fft2, ifft2 are broken or the mistake is mine
+
+    I expect it to be slower due to the python iteration requied, 
+    but hopefully we can get it to return real values instead of complex
+    (slow, but correct)
+
+    we use a Tukey window for both lnBSN and lnBCI separately
+
+    NOTE: 
+        this assumes that lnBSN, lnBCI, lnProb are all numpy.ndarray objects with the same shape and the structure expected for something like matplotlib.pyplot.contours
+        rho2Ao, rho2Bo, params, frac are used to determine the expected sampling errors
+        tukey_alpha is used to constructe a windowing function used in the FFT. We do not use a windowing function in the iFFT because it probably isn't necessary (the convolution kernels should kill most of that stuff)
+    '''
+    ### compute the probability instead of taking it in as an argument
+    N = np.prod(lnBSN.shape)
+    lnProb = lnProb_lnBSNlnBCI_given_rhoA2orhoB2o(
+        lnBSN.flatten(),
+        lnBCI.flatten(),
+        rhoA2o*np.ones(N, dtype=float),
+        rhoB2o*np.ones(N, dtype=float),
+        params,
+        f_tol=f_tol
+    ).reshape(lnBSN.shape)
+
+    ### FFT lnProb    
+    max_lnProb = np.max(lnProb) ### do everything releative to the maximum value to improve numerical precision
+    lnProb -= max_lnProb
+
+    # windowing function
+    size_lnBSN, size_lnBCI = lnProb.shape ### get the dimensionality of lnProb
+    win = np.outer( tukey(size_lnBSN, tukey_alpha), tukey(size_lnBCI, tukey_alpha) )
+
+    # determine conjugate variable values corresponding to FFT array
+    frq_lnBSN, frq_lnBCI = np.meshgrid(
+        np.fft.fftfreq(size_lnBSN, d=lnBSN[0,1]-lnBSN[0,0]),
+        np.fft.fftfreq(size_lnBCI, d=lnBCI[1,0]-lnBCI[0,0]),
+    )
+
+    # actual FFT
+    raise NotImplementedError, 'need to handle fft over multiple dimensions by hand'
+
+    ### multiply by Fourier conjugates of the sampling error distributions
+    lnFFT_lnBSNerr = lnFFTProb_samplingError(frq_lnBSN+frq_lnBCI, sigma_lnBSN(rhoA2rhoB2_to_rho2eta2(rhoA2o, rhoB2o)[0], params, frac=frac))
+    max_lnFFT_lnBSNerr = np.max(lnFFT_lnBSNerr) ### remove this for numerical precision
+    lnFFT_lnBSNerr -= max_lnFFT_lnBSNerr
+
+    lnFFT_lnBCIerr = lnFFTProb_samplingError(frq_lnBCI, sigma_singles(rhoA2o, rhoB2o, params, frac=frac))
+    max_lnFFT_lnBCIerr = np.max(lnFFT_lnBCIerr) ### remove for numerical precision
+    lnFFT_lnBCIerr -= max_lnFFT_lnBCIerr
+
+    # multiply by fourier transform of distributions
+    fftProb *= np.exp(lnFFT_lnBSNerr + lnFFT_lnBCIerr)
+
+    ### iFFT the result
+    raise NotImplementedError, 'need to handle ifft over multiple dimensions by hand'
+
+    ### take log and add back the factors that I kept out for precision
+    return np.log(result) + max_lnProb + max_lnFFT_lnBSNerr + max_lnFFT_lnBCIerr
+
+def convolve_samplingErrors( lnBSN, lnBCI, rhoA2o, rhoB2o, params, frac=0.01, tukey_alpha=0.1, f_tol=1e-10 ):
     '''
     marginalize over sampling errors numerically via spectral convolution
     we use a Tukey window for both lnBSN and lnBCI separately
@@ -352,6 +440,17 @@ def convolve_samplingErrors( lnBSN, lnBCI, lnProb, rhoA2o, rhoB2o, params, frac=
         rho2Ao, rho2Bo, params, frac are used to determine the expected sampling errors
         tukey_alpha is used to constructe a windowing function used in the FFT. We do not use a windowing function in the iFFT because it probably isn't necessary (the convolution kernels should kill most of that stuff)
     '''
+    ### compute the probability instead of taking it in as an argument
+    N = np.prod(lnBSN.shape)
+    lnProb = lnProb_lnBSNlnBCI_given_rhoA2orhoB2o(
+        lnBSN.flatten(), 
+        lnBCI.flatten(), 
+        rhoA2o*np.ones(N, dtype=float), 
+        rhoB2o*np.ones(N, dtype=float), 
+        params, 
+        f_tol=f_tol
+    ).reshape(lnBSN.shape)
+    
     ### FFT lnProb    
     max_lnProb = np.max(lnProb) ### do everything releative to the maximum value to improve numerical precision
     lnProb -= max_lnProb
@@ -392,9 +491,31 @@ def convolve_samplingErrors( lnBSN, lnBCI, lnProb, rhoA2o, rhoB2o, params, frac=
     ### take log and add back the factors that I kept out for precision
     return np.log(result) + max_lnProb + max_lnFFT_lnBSNerr + max_lnFFT_lnBCIerr
 
-'''
-Define marignalization routines
-    - marginalize over sampling errors
-        - direct estimation of the integral
-        - importance sampling
-'''
+def importanceSample_samplingErrors( lnBSN, lnBCI, rhoA2o, rhoB2o, params, frac=0.01, f_tol=1e-10, Nsamp=100 ):
+    '''
+    marginalize over sampling errors via importance sampling
+
+    we draw Nsamp samples at each point of the grid represented by (lnBSN, lnBCI)
+    NOTE:
+        this assumes (lnBSN, lnBCI) are each 1D numpy.ndarray objects
+    '''
+    # get samples from normal distributions
+    N = len(lnBSN)
+    delta1 = np.random.randn(N, Nsamp)*sigma_lnBSN(rhoA2rhoB2_to_rho2eta2(rhoA2o, rhoB2o)[0], params, frac=frac)
+    delta2 = np.random.randn(N, Nsamp)*sigma_singles(rhoA2o, rhoB2o, params, frac=frac)
+
+    # shift the sample points by the samples of sampling errors
+    lnBSN = np.outer(lnBSN, np.ones(Nsamp, dtype=float)) + delta1
+    lnBCI = np.outer(lnBCI, np.ones(Nsamp, dtype=float)) + delta1 + delta2
+
+    # compute lnProb for all these points, reshape, and marginalize
+    n = np.prod(lnBSN.shape)
+    rhoA2o *= np.ones(n, dtype=float)
+    rhoB2o *= np.ones(n, dtype=float)
+    return sumLogs(lnProb_lnBSNlnBCI_given_rhoA2orhoB2o(lnBSN.flatten(), lnBCI.flatten(), rhoA2o, rhoB2o, params, f_tol=f_tol).reshape(N, Nsamp), axis=1) - np.log(Nsamp)
+
+def directIntegrate_sampleErrors( lnBSN, lnBCI, rhoA2o, rhoB2o, params, frac=0.01, f_tol=1e-10 ):
+    '''
+    NOT IMPLEMENTED. Will almost certainly be slower than our other approaches, so perhaps we shouldn't bother...
+    '''
+    raise NotImplementedError, 'this will almost certainly be slow, so perhaps we should not bother?'
